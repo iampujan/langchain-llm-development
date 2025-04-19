4bbadaa68b7d0334f268a2fbc389eabef5eac643
Pujan Thapa <iampujan@outlook.com>
2025-04-19 14:10:17 +0200

    get_completion function added for the response to the prompt.

--- prompt_parser.py
+++ prompt_parser.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from langchain-llmdev!")
+from openai import OpenAI
+import os
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
+# Set OpenAI API key
+
+MODEL_NAME = os.getenv("MODEL_NAME")
+MODEL_API_URL = os.getenv("MODEL_API_URL")
+client = OpenAI(api_key=os.getenv("MODEL_API_KEY"), base_url=MODEL_API_URL)
 
 
-if __name__ == "__main__":
-    main()
+def get_completion(prompt):
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        model=MODEL_NAME,
+        messages = messages,
+    )
+    return response.choices[0].message.content
+
+print(get_completion("What is 1+1?"))
